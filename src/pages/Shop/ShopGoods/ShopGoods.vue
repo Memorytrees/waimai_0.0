f5f5aad8e18dbb5a2a76fc5886754187859c6835
benabree.he@gmail.com <benabree.he@gmail.com>
2018-11-25 11:17:48 +0800

    0000

---
+++ src/pages/Shop/ShopGoods/ShopGoods.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+       ShopGoods
+   </div>
+</template>
+
+<script>
+   export default {
+       data () {
+            return {
+
+            }
+       },
+       components: {
+
+       }
+   }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+ 
+</style>
