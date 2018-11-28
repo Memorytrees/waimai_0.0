7fc5ef79930d051f20bddf17749ab815a3bfd9db
benabree.he@gmail.com <benabree.he@gmail.com>
2018-11-28 11:11:56 +0800

    0100

--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -18,29 +18,34 @@
                     </div>
                 </div>
             </div>
-            <div class="shopcart-list" v-show="listShow">
-                <div class="list-header">
-                    <h1 class="title">购物车</h1>
-                    <span class="empty">清空</span>
+            <transition name="move">
+                <div class="shopcart-list" v-show="listShow">
+                    <div class="list-header">
+                        <h1 class="title">购物车</h1>
+                        <span class="empty" @click="cleanCart">清空</span>
+                    </div>
+                    <div class="list-content">
+                        <ul>
+                            <li class="food" v-for="(food, index) in cartFoods" :key="index">
+                                <span class="name">{{ food.name }}</span>
+                                <div class="price"><span>￥{{ food.price }}</span></div>
+                                <div class="cartcontrol-wrapper">
+                                    <CartControl :food="food"/>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="list-content">
-                    <ul>
-                        <li class="food" v-for="(food, index) in cartFoods" :key="index">
-                            <span class="name">{{ food.name }}</span>
-                            <div class="price"><span>￥{{ food.price }}</span></div>
-                            <div class="cartcontrol-wrapper">
-                                <CartControl :food="food"/>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            </transition>
+            
         </div>
         <div class="list-mask" v-show="listShow" @click="toggleShow"></div>
     </div>
 </template>
 
 <script>
+    import { MessageBox } from 'mint-ui'
+    import BScroll from 'better-scroll'
     import { mapState, mapGetters } from 'vuex'
     import CartControl from '../CartControl/CartControl.vue'
     export default {
@@ -75,12 +80,35 @@
                     this.isShow = false
                     return false
                 } 
+                if(this.isShow) {
+                    this.$nextTick( ()=> {
+                        // 实现BScroll的实例是一个单例
+                        if(!this.scroll) {
+                            this.scroll = new BScroll('.list-content', {
+                            click: true
+                            })
+                        } else {
+                            this.scroll.refresh() // 让滚动条刷新一下：重新统计内容的高度
+                        }
+                        
+                    })
+                }
                 return this.isShow;
             }
         },
         methods: {
             toggleShow () {
-                this.isShow = !this.isShow
+                // 只有当总数量大于0时切换
+                if(this.totalCount>0) {
+                    this.isShow = !this.isShow
+                }
+                
+            },
+
+            cleanCart () {
+                MessageBox.confirm('确定清空购物车吗').then(action => {
+                    this.$store.dispatch('clearCart')
+                }, () => {});
             }
         },
         components: {
@@ -198,9 +226,9 @@
         z-index -1
         width 100%
         transform translateY(-100%)
-        &.swipe-enter-active, &.swipe-leave-active
-            transition transform .3s
-        &.swipe-enter, &.swipe-leave-to
+        &.move-enter-active, &.move-leave-active
+            transition transform .5s
+        &.move-enter, &.move-leave-to
             transform translateY(0)
         .list-header
             height 40px
