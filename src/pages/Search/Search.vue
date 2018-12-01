a6b61c6f5afc14cdb37a0f9f2a205bd351064254
benabree.he@gmail.com <benabree.he@gmail.com>
2018-12-01 16:03:12 +0800

    0111

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -6,47 +6,66 @@
             <input type="search" placeholder="请输入商家名称" class="search_input" v-model="keyword">
             <input type="submit" class="search_submit">
         </form>
-        <section class="list">
+        <section class="list" v-if="!noSearchShops">
             <ul class="list_container">
-                <li class="list_li">
+                <router-link :to="{path:'/shop', query:{id:item.id}}" tag="li" v-for="item in searchShops" :key="item.id" class="list_li">
                     <section class="item_left">
-                        <img src="http://cangdu.org:8001/img/16265a70fe27854.jpg" class="restaurant_img">
+                        <img :src="imgBaseUrl + item.image_path" class="restaurant_img">
                     </section>
                     <section class="item_right">
                         <div class="item_right_text">
                             <p>
-                                <span>aaa</span>
+                                <span>{{item.name}}</span>
                             </p>
-                            <p>月售 671 单</p>
-                            <p>20 元起送 / 距离 1058.2 公里</p>
+                            <p>月售 {{item.month_sales||item.recent_order_num}} 单</p>
+                            <p>{{item.delivery_fee||item.float_minimum_order_amount}} 元起送 / 距离{{item.distance}}</p>
                         </div>
                     </section>
-                </li>
-                <li class="list_li">
-                    <section class="item_left">
-                        <img src="http://cangdu.org:8001/img/16265a70fe27854.jpg" class="restaurant_img">
-                    </section>
-                    <section class="item_right">
-                        <div class="item_right_text">
-                            <p>
-                                <span>aaa</span>
-                            </p>
-                            <p>月售 671 单</p>
-                            <p>20 元起送 / 距离 1058.2 公里</p>
-                        </div>
-                    </section>
-                </li>
+                </router-link>
+         
             </ul>
         </section>
+        <div class="search_none" v-else>很抱歉！无搜索结果</div>
     </section>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import HeaderTop from "../../components/HeaderTop/HeaderTop.vue"
 export default {
     data() {
-        return {};
+        return {
+            keyword: '',
+            imgBaseUrl: 'http://cangdu.org:8001/img',
+            noSearchShops: false
+        };
     },
+
+    computed: {
+        ...mapState(['searchShops'])
+    },
+
+    methods: {
+        search () {
+            // 得到搜索关键字
+            const keyword = this.keyword.trim()
+            // 进行搜索
+            if(keyword) {
+                this.$store.dispatch('searchShops', keyword)
+            }
+        }
+    },
+
+    watch: {
+        searchShops (value) {
+            if(!value.length) { // 没有数据
+                this.noSearchShops = true
+            } else {// 有数据
+                this.noSearchShops = false
+            }
+        }
+    },
+    
     components: {
         HeaderTop
     }
@@ -55,7 +74,7 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixins.stylus"
-    .search
+.search
     width 100%
     height 100%
     overflow hidden
